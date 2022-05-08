f72b285d338d7a6c61f39b645de0901b50869cb7
chambg02 <chambg02@gmail.com>
2022-05-08 20:06:55 +0800

    Complete exercises 08 and 09

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,43 @@
-const add = function() {
-	
+const add = function(a, b) {
+	let addition = a + b;
+  return addition;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  let substraction = a - b;
+  return substraction;
 };
 
-const sum = function() {
-	
+const sum = function(sumArray) {
+	let initialValue = 0;
+  let sumOfArray = sumArray.reduce(
+    (previousValue, currentValue) => previousValue + currentValue, initialValue);
+return sumOfArray;
 };
 
-const multiply = function() {
-
+const multiply = function(multiplyArray) {
+  let initValue = 1;
+  let multiplicationOfArray = multiplyArray.reduce(
+    (prevValue, currValue) => prevValue * currValue, initValue);
+return multiplicationOfArray;
 };
 
-const power = function() {
-	
+
+const power = function(a, b) {
+	let powerOf = a**b;
+  return powerOf;
 };
 
-const factorial = function() {
-	
-};
+
+const factorial = function(a) {
+  let array =[];
+  for (let i = 0; i < a; i++) {
+    array[i] = i+1;
+  }
+  let multiplicationArray = array.reduce(
+    (preValue, curValue) => preValue * curValue, 1);
+  return multiplicationArray;
+  };
 
 // Do not edit below this line
 module.exports = {
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,25 @@
-const palindromes = function () {
+const palindromes = function (words) {
+    let letterArray = [];
+    for (let i = 0; i < words.length; i++) {
+        if (!/[^a-zA-Z]/.test(words[i]) == 1) {
+            letterArray[i] = words.charAt(i).toLowerCase();
 
+    }
+    }
+return letterArray.join("") === letterArray.reverse().join("");
 };
+
+/* OFFICIAL SOLUTION
+const palindromes = function(string) {
+  processedString = string.toLowerCase().replace(/[^a-z]/g, "");
+  return (
+    processedString
+      .split("")
+      .reverse()
+      .join("") == processedString
+  );
+};
+*/
 
 // Do not edit below this line
 module.exports = palindromes;
